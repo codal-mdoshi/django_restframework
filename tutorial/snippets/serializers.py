489c0e89b51aba2aa876d1ada6d817e7cd2d5c7a
marmik <mdoshi@codal.com>
2016-07-05 13:12:50 +0530

    viewsets and routers

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,22 +4,6 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-# class SnippetSerializer(serializers.ModelSerializer):
-#     owner = serializers.ReadOnlyField(source='owner.username')
-#
-#     class Meta:
-#         model = Snippet
-#         fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
-#
-#
-# class UserSerializer(serializers.ModelSerializer):
-#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-#
-#     class Meta:
-#         model = User
-#         fields = ('id', 'username', 'snippets')
-
-
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
